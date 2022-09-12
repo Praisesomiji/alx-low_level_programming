95470e4c59f78da50b35ec414786485687d173aa
Praisesomiji <praisesomiji@gmail.com>
2022-09-12 12:20:24 +0000

    Submit task 4 and 5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+	/* Initialize strings */
+	char str[] = "Programming is like building a multilingual puzzle";
+	
+	/* Write to stdout */
+	puts(str);
+
+	return 0;
+}	
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+	/* Initialize strings */
+	char str[] = "with proper grammar, but the outcome is a piece of art,";
+	
+	/* Writing to stdout */
+	printf("%s\n",str);
+
+	return 0;
+}
