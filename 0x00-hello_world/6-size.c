beb71b049869f481527bbfb45d4a5388d1fc7c06
Praisesomiji <praisesomiji@gmail.com>
2022-09-13 02:19:56 +0000

    Remove return value from task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,16 +3,13 @@
  * main - Entry point
  *
  * Print sizes of c data types
- *
- * Return: Always 0 (Success)
  */
-int main(void)
+void main(void)
 {
-	fprintf(stderr, "Size of a char: %lu byte(s)\n", sizeof(char));
-	fprintf(stderr, "Size of a int: %lu byte(s)\n", sizeof(int));
-	fprintf(stderr, "Size of a long int: %lu byte(s)\n", sizeof(long int));
-	fprintf(stderr, "Size of a long long int: %lu byte(s)\n", sizeof(long long int));
-	fprintf(stderr, "Size of a float: %lu byte(s)\n", sizeof(float));
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of a int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 
-	return (0);
 }
