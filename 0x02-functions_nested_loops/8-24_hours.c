6de02cfa514aac4610665f60c0a75d32396d1981
Praisesomiji <praisesomiji@gmail.com>
2023-03-22 19:34:08 +0100

    Fix betty styling in 8 and 7

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 /**
  * jack_bauer - prints every minute of the day of Jack Bauer
  * starting from 00:00 to 23:59
@@ -8,8 +7,8 @@
 void jack_bauer(void)
 {
 	int i, j;
-	
-	
+
+
 	i = 0;
 
 
