a7b0c49b229482181f4a2a7a619a538347234cc8
Praisesomiji <praisesomiji@gmail.com>
2023-06-10 04:05:39 +0100

    Improve style

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include  <string.h>
+
+int positive(char*, int*);
 
 /**
  * main - prints sum of positive numbers
@@ -8,24 +11,41 @@
  *
  * Return: success(0).
  */
-
 int main(int argc, char *argv[])
 {
-	int sum, i;
+	int sum;
+	int i;
 
 	for (i = 1, sum = 0; i < argc; i++)
 	{
-		char *m = argv[i];
-		int n = atoi(m);
-
-		if (n == 0 && strcmp(m, "0") != 0)
+		if (positive(argv[i], &sum) != 0)
 		{
-			printf("Error\n");
 			return (1);
 		}
-		sum += n;
 	}
 	printf("%i\n", sum);
+
+	return (0);
+}
+
+/**
+ * positive - checks if a given digit is positive
+ * @m: given arg
+ * @sum: accumulator
+ *
+ * Return: success(0) or failure(0).
+ */
+int positive(char *m, int *sum)
+{
+	int n = atoi(m);
+
+	if (n == 0 && strcmp(m, "0") != 0)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	(*sum) += n;
 
 	return (0);
 }
