01e2d1c9af8d23c3b3e58b39e1e3d9844e789375
Praisesomiji <praisesomiji@gmail.com>
2023-06-13 18:46:07 +0100

    Write a function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,6 +12,11 @@
 	char *s;
 	int len, i;
 
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
 	len = 0;
 	while (str[len] != '\n')
 	{
@@ -19,11 +24,15 @@
 	}
 	s = malloc(sizeof(char) * (len + 1));
 	
+	if (s = NULL)
+	{
+		return (NULL);
+	}
 
+	for (i = 0; i < len + 1; i++)
+	{
+		s[i] = str[i];
+	} 
 
-	// allocate memory for array of size [size]
-// if error, return NULL
-
-// for each byte allocated, initialize it to char [c]
-// return pointer to allocated memory 
+	return (s);
 }
