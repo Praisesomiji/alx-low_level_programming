db9b7316e8a6e981a1296beb715f2656862b1a76
Praisesomiji <praisesomiji@gmail.com>
2023-06-13 19:56:51 +0100

    Fix: typo

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,7 +3,7 @@
 
 int _strlen(char *str);
 
-/** 
+/**
 * str_concat - concatenate two strings
  * @s1: first string
  * @s2: last string
@@ -12,33 +12,34 @@ int _strlen(char *str);
  */
 char *str_concat(char *s1, char *s2)
 {
-        char *s;
-        int l1, l2, l, i;
+	char *s;
+	int l1, l2, l, i;
 
-        l1 = _strlen(s1);
+	l1 = _strlen(s1);
 	l2 = _strlen(s2);
 	l = l1 + l2;
 
-        s = malloc(sizeof(char) * (l + 1));
-        if (s == NULL)
-        {
-                return (NULL);
-        }
+	s = malloc(sizeof(char) * (l + 1));
+	if (s == NULL)
+	{
+		return (NULL);
+	}
 
-        for (i = 0; i < l1 + 1; i++)
-        {
-                s[i] = s1[i];
-        }
+	for (i = 0; i < l1 + 1; i++)
+	{
+		s[i] = s1[i];
+	}
 	for (; i < l; i++)
 	{
 		s[i] = s2[i];
 	}
 	s[i] = '\0';
 
-        return (s);
+	return (s);
 }
 
-/** _strlen - find the length of a string
+/** 
+ * _strlen - find the length of a string
  * @str: a string
  *
  * Return: length of string.
@@ -53,10 +54,10 @@ int _strlen(char *str)
 	}
 
 	len = 0;
-        while (str[len] != '\0')
-        {
-                len++;
-        }
+	while (str[len] != '\0')
+	{
+		len++;
+	}
 
 	return (len);
 }
