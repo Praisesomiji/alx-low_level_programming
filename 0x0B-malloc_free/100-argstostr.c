91f04b20bdb898498aba642ea19af70f8a9e3901
Praisesomiji <praisesomiji@gmail.com>
2023-06-14 00:54:22 +0100

    Write a function that concatenates all the arguments of your program

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int _strlen(char *str);
+
+/**
+* argstostr - concatenate program args
+ * @ac: args count
+ * @av: args vector
+ *
+ * Return: concatenated string.
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, len;
+	char *str;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+
+	len = 0;
+	for (i = 0; i < ac; i++)
+	{
+		len += _strlen(av[i]) + 2;
+	}
+
+	str = malloc(sizeof(char) * len);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0, k = 0; i < ac; i++, k++)
+	{
+		l = _strlen(av[i]) + 1;
+
+		for (j = 0; j < l; j++, k++)
+		{
+			*(str + k) = av[i][j];
+		}
+		*(str + k) = '\n';
+	}
+	
+	return (str);
+}
+
+/**
+ * _strlen - find the length of a string
+ * @str: a string
+ *
+ * Return: length of string.
+ */
+int _strlen(char *str)
+{
+	int len;
+
+	if (str == NULL)
+	{
+		return (0);
+	}
+
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	return (len);
+}
