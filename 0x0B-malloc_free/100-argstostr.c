780c8190eefd4ab13bd4893ce08b571d66da39e1
Praisesomiji <praisesomiji@gmail.com>
2023-06-14 01:03:18 +0100

    Fix: declare variables before init

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -26,7 +26,7 @@ char *argstostr(int ac, char **av)
 		len += _strlen(av[i]) + 1;
 	}
 
-	str = malloc(sizeof(char) * len);
+	str = malloc(sizeof(char) * len + 1);
 	if (str == NULL)
 	{
 		return (NULL);
