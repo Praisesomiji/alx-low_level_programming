c3eddce6fca9be09af52210d7f614b7d671569bd
Praisesomiji <praisesomiji@gmail.com>
2023-06-15 22:00:31 +0100

    Write a function that concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,1 +1,71 @@
-#include
+#include <stdio.h>
+#include <stdlib.h>
+
+int _strlen(char *str);
+
+/**
+ * string_nconcat - concatenate two strings
+ * @s1: a string
+ * @s2: a string
+ * @n: number of chars to copy from second string
+ *
+ * Return: pointer to allocated space in memory
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int size, i, j;
+	int l1, l2;
+	char *str;
+
+	l1 = _strlen(s1);
+	l2 = _strlen(s2);
+
+	if (n >= l2)
+	{
+		size = l1 + l2;
+	}
+	else
+	{
+		size = l1 + l2;
+	}
+
+	str = malloc(sizeof(*str) * size + 1);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < l1; i++)
+	{
+		str[i] = s1[i];
+	}
+	for (j = 0; i < size; i++, j++)
+	{
+		str[i] = s2[i];
+	}
+	s[i] = '\0';
+
+	return (str);
+}
+
+/**
+ * _strlen - find the length of a string
+ * @str: a string
+ *
+ * Return: length of string
+ */
+int _strlen(char *str)
+{
+	int len;
+
+	len = 0;
+	if (str != NULL)
+	{
+		while (str[len] != '\0')
+		{
+			len++
+		}
+	}
+	return (len);
+}
+
