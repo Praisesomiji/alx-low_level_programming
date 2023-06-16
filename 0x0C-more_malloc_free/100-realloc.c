11355c019d6126e36a2cc666e6756277ff30bc5b
Praisesomiji <praisesomiji@gmail.com>
2023-06-16 02:29:35 +0100

    FIx: bugs

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -18,11 +18,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_size == old_size)
 	{
-		if (ptr != NULL)
-		{
-			return (ptr);
-		}
-
+		return (ptr);
 	}
 
 	if (new_size == 0)
