5ddf9cfcfca325f9fe4ee888c06e01b690b18004
Praisesomiji <praisesomiji@gmail.com>
2023-06-20 01:26:43 +0100

    Pset4: Write a function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,4 +11,20 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	dog_t *d;
+
+	if (name == NULL || owner == NULL)
+	{
+		return (NULL);
+	}
+	d = malloc(dog_t);
+	if (d == NULL)
+	{
+		return (NULL);
+	}
+	d.name = name;
+	d.age = age
+	d.owner = owner;
+
+	return (d);
 }
