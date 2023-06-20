c506a26e49ad454d466c4f1d6b53a4f2cf58e147
Praisesomiji <praisesomiji@gmail.com>
2023-06-21 00:21:45 +0100

    Fix copy

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include "dog.h"
 
+int _storcpy(char *dest, char *src);
+
 /**
  * new_dog - create new dog
  * @name: name
@@ -21,9 +23,37 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (d == NULL)
 		return (NULL);
 
-	(*d).name = name;
+	_storcpy((*d).name, name);
+	_storcpy((*d).owner, owner);
+
 	(*d).age = age;
-	(*d).owner = owner;
 
 	return (d);
+}
+
+/**
+ * _storcpy - store a copy off src in dest
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: Success(0).
+ */
+int _storcpy(char *dest, char *src)
+{
+	int i, len;
+
+	if (src == NULL)
+		return (98);
+
+	for (len = 1; src[len] != '\0'; len++)
+		;
+	dest = malloc(sizeof(*src) * len);
+	if (dest == NULL)
+		return (98);
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (0);
 }
