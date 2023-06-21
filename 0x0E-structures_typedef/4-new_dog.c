d407641e33ccf47ab5a0c925ce1dd13b6d3fa479
Praisesomiji <praisesomiji@gmail.com>
2023-06-21 12:40:25 +0100

    Pset 4: Fixed not copying string

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include "dog.h"
 
-int _storcpy(char *dest, char *src);
+char *_strncpy(char *dest, char *src, int n);
+int _strlen(char *str);
 
 /**
  * new_dog - create new dog
@@ -15,45 +16,69 @@ int _storcpy(char *dest, char *src);
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
+	int l0, l1;
 
 	if (name == NULL || owner == NULL)
-		return (NULL);
+		exit(98);
 
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
-		return (NULL);
+		exit(98);
 
-	_storcpy((*d).name, name);
-	_storcpy((*d).owner, owner);
+	l0 = _strlen(name);
+	d->name = malloc(sizeof(char) * (l0 + 1));
+	if (d->name == NULL)
+		exit(98);
 
-	(*d).age = age;
+	l1 = _strlen(owner);
+	d->owner = malloc(sizeof(char) * (l1 + 1));
+	if (d->owner == NULL)
+		exit(98);
+
+	if (_strncpy(d->name, name, l0) == NULL)
+		exit(98);
+
+	if (_strncpy(d->owner, owner, l1) == NULL)
+		exit(98);
+
+	d->age = age;
 
 	return (d);
 }
 
 /**
- * _storcpy - store a copy off src in dest
+ * _strlen - find length of string
+ * @str: string
+ *
+ * Return: lenght of string.
+ */
+int _strlen(char *str)
+{
+	int len;
+
+	for (len = 0; str[len] != '\0'; len++)
+		;
+	return (len);
+}
+
+/**
+ * _strncpy - store a copy off src in dest
  * @dest: destination string
  * @src: source string
+ * @n: number of chars to be copied
  *
  * Return: Success(0).
  */
-int _storcpy(char *dest, char *src)
+char *_strncpy(char *dest, char *src, int n)
 {
-	int i, len;
+	int i;
 
-	if (src == NULL)
-		return (98);
+	if (dest == NULL || src == NULL)
+		return (NULL);
 
-	for (len = 1; src[len] != '\0'; len++)
-		;
-	dest = malloc(sizeof(*src) * len);
-	if (dest == NULL)
-		return (98);
-
-	for (i = 0; i < len; i++)
+	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
 	}
-	return (0);
+	return (dest);
 }
