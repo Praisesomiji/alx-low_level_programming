6edc52035c6a21b9b3f6da53dfb5bd4c17f1ac18
Praisesomiji <praisesomiji@gmail.com>
2023-07-04 03:55:24 +0100

    PSET 0: Try fixes

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,6 +11,9 @@ size_t print_list(const list_t *h)
 	size_t count = 0;
 	const list_t *this = h;
 
+	if ((*h) == NULL)
+		return (0);
+
 	while (this != NULL)
 	{
 		printf("[%i] %s\n", this->len, this->str);
