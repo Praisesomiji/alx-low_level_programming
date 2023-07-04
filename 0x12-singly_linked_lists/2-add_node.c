52beb8260b7360ff9dfb2e677d570e92fc519190
Praisesomiji <praisesomiji@gmail.com>
2023-07-04 01:34:25 +0100

    Write a function that adds a new node at the beginning of a list_t list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,5 +9,25 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
+	list_t *this;
+	list_t *new;
+	const char data = NULL;
 
+	if (head == NULL)
+		exit(98);
+
+	this = *head;
+	data = strdup(str);
+	if (data == NULL)
+		exit(98);
+
+	new->str = data;
+	new->len = _strlen(data);
+	if (this != NULL)
+		new->next = this;
+	else
+		new->next = NULL;
+	
+	*head = new;
+	return (new);
 }
