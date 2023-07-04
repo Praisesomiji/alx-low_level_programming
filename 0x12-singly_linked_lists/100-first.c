4ad0ca47229678cde1ad36fa9ce1bdaaf1c1e50f
Praisesomiji <praisesomiji@gmail.com>
2023-07-04 04:24:37 +0100

    PSET 100: Write a function that prints a text before the main function is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * preliminary - prints some text
+ *
+ * Return: Nothing.
+ */
+void __attribute__((constructor)) preliminary()
+{
+
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
