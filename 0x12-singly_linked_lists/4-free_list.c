617d7822489522f2ce5aff1d3f160b3445672134
Praisesomiji <praisesomiji@gmail.com>
2023-07-04 03:49:35 +0100

    PSET 4: Write a function that frees a list_t list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -6,4 +6,15 @@
  *
  * Return: Nothing.
  */
+void free_list(list_t *head)
+{
+	list_t = *temp;
 
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+}
