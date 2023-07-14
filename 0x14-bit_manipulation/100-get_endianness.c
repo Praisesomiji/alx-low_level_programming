bd2813d0b16971bfbdd2ccc88cf03129461218da
Praisesomiji <praisesomiji@gmail.com>
2023-07-14 09:53:15 +0100

    PSET 6: Fix Styling

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -12,7 +12,7 @@ int get_endianess(void)
 
 	/* Convert p to int to p to char to access a byte at a time */
 	n = 0x0A0B0C0D;
-	np = (unsigned char*)&n;
+	np = (unsigned char *)&n;
 	/* Big endian if the most significant byte is stored first */
 	if (*np == 0x0A)
 		return (0);
