68d9c558fc90cf808a029c80587141e85691aa51
Praisesomiji <praisesomiji@gmail.com>
2023-07-14 09:50:33 +0100

    PSET 6: Write a function that checks the endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * get_endianess - checks the endianness
+ *
+ * Return: 0 if big endian, 1 if little indian.
+ */
+int get_endianess(void)
+{
+	unsigned int n;
+	unsigned char *np;
+
+	/* Convert p to int to p to char to access a byte at a time */
+	n = 0x0A0B0C0D;
+	np = (unsigned char*)&n;
+	/* Big endian if the most significant byte is stored first */
+	if (*np == 0x0A)
+		return (0);
+	/* Little endian, most significant stored last */
+	return (1);
+}
