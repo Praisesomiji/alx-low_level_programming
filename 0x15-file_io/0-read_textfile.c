447144c7689e182ca619b33e9edfa02ca745ac5b
Praisesomiji <praisesomiji@gmail.com>
2023-07-18 01:59:25 +0100

    PSET 0: Close file descriptor and Free malloced buffer

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -29,6 +29,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	/* if write fails or doesnt write expected no of bytes, return 0 */
 	written = write(STDOUT_FILENO, buff, read(fd, buff, letters));
 
+	free(buff);
+	close(fd);
+
 	/* returns the actual number of letters it could read and print */
 	return (written);
 }
