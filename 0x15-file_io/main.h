f24418713cd008dce7165129a17f870a14d59f96
Praisesomiji <praisesomiji@gmail.com>
2023-07-18 02:48:48 +0100

    PSET 1: Fix typos

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,7 +7,7 @@
 #include <fcntl.h>
 
 int _putchar(char c);
-size_t _strlen(char *str)
+size_t _strlen(char *str);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 
