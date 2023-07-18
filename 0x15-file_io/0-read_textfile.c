9dd288ffddfa09c116b2baeb176ed5f7b7a67f89
Praisesomiji <praisesomiji@gmail.com>
2023-07-18 01:56:15 +0100

    PSET 0: FIx Styling:

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -28,7 +28,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	/* if write fails or doesnt write expected no of bytes, return 0 */
 	written = write(STDOUT_FILENO, buff, read(fd, buff, letters));
-	
+
 	/* returns the actual number of letters it could read and print */
 	return (written);
 }
