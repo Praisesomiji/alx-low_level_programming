22fa6853b33640e1c596a9be88f03c4551dae784
Praisesomiji <praisesomiji@gmail.com>
2023-07-18 03:45:16 +0100

    PSET 1: Fix assigned permisssions

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC);
+	fd = open(filename,  O_WRONLY | O_TRUNC | O_CREAT, 0600);
 	if (fd < 0)
 		return (-1);
 
@@ -39,7 +39,7 @@ int create_file(const char *filename, char *text_content)
  */
 size_t _strlen(char *str)
 {
-	int count;
+	size_t count;
 
 	if (!str)
 		return (0);
