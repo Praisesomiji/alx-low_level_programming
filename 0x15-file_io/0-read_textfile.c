2a0e711d5e86b77abd583b00ae203cc501a591de
Praisesomiji <praisesomiji@gmail.com>
2023-07-18 01:54:40 +0100

    PSET 0: Test without one of the given conditional checks

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,7 +11,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	char *buff;
-	int written;
+	ssize_t written;
 
 	/* if filename is NULL return 0 */
 	if (!filename)
@@ -28,9 +28,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	/* if write fails or doesnt write expected no of bytes, return 0 */
 	written = write(STDOUT_FILENO, buff, read(fd, buff, letters));
-	if (written < letters)
-		return (0);
-
+	
 	/* returns the actual number of letters it could read and print */
 	return (written);
 }
